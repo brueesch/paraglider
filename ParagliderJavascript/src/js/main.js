2231e2c8001da7f52b8a35125f7c981b0bb0eba3
brueesch <c.brueesch@yahoo.de>
2014-11-17 03:35:31 +0100

    Basic Structure / First Animation
    Created the first structure of google earth.
    Created first animation

---
+++ ParagliderJavascript/src/js/main.js
@@ -0,0 +1,98 @@
+/*
+ * -------------------------------------------------------
+ * Initializing Google Earth
+ * -------------------------------------------------------
+ */
+
+var ge;
+var timer;
+
+google.load("earth", "1", false);
+
+//Creates a new instance of google earth.
+function init() {
+	// Param: div id where the earth will appear, function to call by success,
+	// function to call by failure.
+	google.earth.createInstance('map3d', initCB, failureCB);
+	
+}
+
+//initialises google earth and set it's visibility to true.
+function initCB(instance) {
+	   ge = instance;
+	   ge.getWindow().setVisibility(true);
+	   initCamera();
+}
+
+function failureCB(errorCode) {
+	alert(errorCode);
+}
+
+
+
+/*
+ * -------------------------------------------------------
+ * Setting up the Camera
+ * -------------------------------------------------------
+ */
+
+//Coordinates of New York City: Latitude: 40.681682 Longitude: -74.038428
+//COordinates of Weisshorn: Latitude: 46.789676 Longitude: 9.638993
+
+function initCamera() {
+	var camera = ge.getView().copyAsCamera(ge.ALTITUDE_RELATIVE_TO_GROUND);
+
+	ge.getOptions().setFlyToSpeed(3.0);
+	camera.setLatitude(40.681682);
+	camera.setLongitude(-74.038428);
+	camera.setTilt(camera.getTilt()+ 90);
+	camera.setAltitude(500);
+	camera.setHeading(45);
+	
+	ge.getView().setAbstractView(camera);
+}
+
+function move() {
+	var camera = ge.getView().copyAsCamera(ge.ALTITUDE_RELATIVE_TO_GROUND);
+
+	// Add 25 degrees to the current latitude and longitude values.
+	camera.setLatitude(camera.getLatitude() + 0.0001);
+	camera.setLongitude(camera.getLongitude() +0.0001);
+
+	// Update the view in Google Earth.
+	ge.getView().setAbstractView(camera);
+	
+    timer = setTimeout("move()",50); // time in milliseconds; this is 5 seconds
+}
+
+function stopMovement() {
+	clearTimeout(timer);	
+}
+
+
+google.setOnLoadCallback(init);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
